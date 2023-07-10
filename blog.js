12148a23385bb42b62d6ee19edf21586e4d8c6bc
Nikhil Tomar <nikhiltomar2690@gmail.com>
2023-07-11 02:21:32 +0530

    All set

---
+++ blog.js
@@ -0,0 +1,75 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
+import { getFirestore, collection,doc,getDoc} from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY-ChTNQK9aqpB4T4uXpG2QMggrZHxHtk",
+  authDomain: "cbid-1a433.firebaseapp.com",
+  projectId: "cbid-1a433",
+  storageBucket: "cbid-1a433.appspot.com",
+  messagingSenderId: "271147078204",
+  appId: "1:271147078204:web:c6cb62ff447458607a6939",
+  measurementId: "G-3K4H36WKBZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get the Firestore reference
+const db = getFirestore(app);
+
+
+
+// Get the documents from the collection
+const collectionRef = collection(db, 'blogs');
+
+// Get the blogId and collectionRef from the query parameters
+const urlParams = new URLSearchParams(window.location.search);
+const selectedBlogId = urlParams.get('blogId');
+const collectionRefPath = decodeURIComponent(urlParams.get('collectionRef'));
+
+// Convert collectionRefPath back to a Firestore CollectionReference
+// const collectionRef = collection(db, collectionRefPath);
+
+// Fetch the blog content based on the selectedBlogId
+fetchBlogData(collectionRef, selectedBlogId)
+  .then((blogData) => {
+    if (blogData) {
+      displayBlogContent(blogData);
+    } else {
+      displayBlogNotFound();
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching blog data:", error);
+    displayBlogNotFound();
+  });
+
+async function fetchBlogData(collectionRef, blogId) {
+  const blogRef = doc(collectionRef, blogId);
+  const docSnapshot = await getDoc(blogRef);
+  if (docSnapshot.exists()) {
+    return docSnapshot.data();
+  }
+  return null;
+}
+
+function displayBlogContent(blogData) {
+  // Update the DOM with the blog content
+  const blogContentEl = document.getElementById("blog-content");
+
+  blogContentEl.classList.add("cards");
+
+  blogContentEl.innerHTML = `
+    <h1 class="about-title">${blogData.title}</h1>
+    <p class="about-p">${blogData.content}</p>
+    <p class="about-date">${blogData.date}</p>
+    <br>
+    <a href="index.html"><strong>Back to Blogs</strong></a>
+  `;
+}
+
+function displayBlogNotFound() {
+  // Update the DOM to display a message for a blog not found
+  const blogContentEl = document.getElementById("blog-content");
+  blogContentEl.innerHTML = `<p>Blog not found.</p>`;
+}
--- blogpage.js
+++ blogpage.js
@@ -18,76 +18,42 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-
 // Get the documents from the collection, sorted by timestamp in descending order
 const collectionRef = collection(db, 'blogs');
-const querySnapshot = await getDocs(query(collectionRef, orderBy('timestamp', 'desc')));
+const querySnapshot = await getDocs(query(collectionRef, orderBy('timestamp', 'asc')));
+// const querySnapshot = await getDocs(collectionRef);
 
 // Clear the cards
-const cardsEl = document.getElementById("cards");
-cardsEl.innerHTML = "";
+const cardsE2 = document.getElementById("cards");
+cardsE2.innerHTML = "";
 
 // Function to open blog.html with the selected blog ID
 function openBlogPage(blogId) {
   window.location.href = `blog.html?blogId=${blogId}`;
 }
 
-// Loop through the documents and create a card for each one, limiting to top 10
-let count = 0;
 querySnapshot.forEach((doc) => {
-  if (count < 10) {
-    const cardEl = document.createElement("div");
-    cardEl.classList.add("cards");
+    const cardE2 = document.createElement("div");
+    cardE2.classList.add("cards");
 
      // Add the document data to the card
-     cardEl.innerHTML = `
+     cardE2.innerHTML = `
      <p class="date">Last Updated ${doc.data().date}</p>
-     <h2 class="card-title">${doc.data().title}</h2>
+     <h2 class="card-title card-link ">${doc.data().title}</h2>
      <p class="card-text">${doc.data().desc} <a href="#" class="card-link"><strong>read more</strong></a></p>
    `;
 
    // Attach event listener to the "read more" link
-   const readMoreLink = cardEl.querySelector(".card-link");
+   const readMoreLink = cardE2.querySelector(".card-link");
    readMoreLink.addEventListener("click", (event) => {
      event.preventDefault();
      openBlogPage(doc.id);
    });
-
     // Add the card to the DOM
-    cardsEl.appendChild(cardEl);
+    cardsE2.appendChild(cardE2);
   }
-  count++;
-});
+);
 
-
-async function search() {
-  // Get the search keyword from the user input
-  const searchKeyword = document.getElementById("searchInput").value;
-
-  // Get the documents from the collection matching the search query
-  const collectionRef = collection(db, 'blogs');
-  const querySnapshot = await getDocs(query(collectionRef, where('title', 'array-contains', searchKeyword)));
-
-  // Clear the search results
-  const searchResultsEl = document.getElementById("searchResults");
-  searchResultsEl.innerHTML = "";
-
-  // Loop through the documents and create a card for each matching blog
-  querySnapshot.forEach((doc) => {
-    const cardEl = document.createElement("div");
-    cardEl.classList.add("cards");
-    console.log(`The search result is ${doc.data().title}`);
-    // Add the document data to the card
-    cardEl.innerHTML = `
-      <p class="date">${doc.data().date}</p>
-      <h2 class="card-title">${doc.data().title}</h2>
-      <p class="card-text">${doc.data().desc} <a href="https://youtube.com" class="card-link"><strong>read more</strong></a></p>
-    `;
-
-    // Add the card to the search results
-    searchResultsEl.appendChild(cardEl);
-  });
-}
 // telling js about the search function
 // document.getElementById("searchButton").addEventListener("click", search);
 document.addEventListener("DOMContentLoaded", function() {
